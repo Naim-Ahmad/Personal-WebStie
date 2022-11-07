96b69d160f4c25f4c8858e07e960fefdeb9d4428
Naim Ahmad <naimahmad2011103@gmail.com>
2022-11-07 18:03:32 +0600

    I have aded my about

--- app.js
+++ app.js
@@ -3,15 +3,29 @@ const navPos = navBar.getBoundingClientRect("nav-bar").top;
 
 let navbarLinks = document.querySelectorAll("nav a");
 
-console.log(navbarLinks);
+window.onload = () => {
+  main();
+};
 
-window.addEventListener("scroll", (event) => {
-  let scrollPos = this.scrollY;
-  if (scrollPos > navPos) {
-    navBar.classList.add("nav-bar");
-  } else {
-    navBar.classList.remove("nav-bar");
-  }
+function main() {
+  window.addEventListener("scroll", (event) => {
+    let scrollPos = window.scrollY;
+    if (scrollPos > navPos) {
+      navBar.classList.add("nav-bar");
+    } else {
+      navBar.classList.remove("nav-bar");
+    }
 
-  navbarLinks.forEach((link) => {});
-});
+    navbarLinks.forEach((link) => {
+      let section = document.querySelector(link.hash);
+      if (
+        scrollPos + 150 > section.offsetTop &&
+        scrollPos + 150 < section.offsetTop + section.offsetHeight
+      ) {
+        link.classList.add("active");
+      } else {
+        link.classList.remove("active");
+      }
+    });
+  });
+}
