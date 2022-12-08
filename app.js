e55a41c58cdd551b0517196daa8cdaccbdd4bdc1
Naim Ahmad <naimahmad201103@gmail.com>
2022-12-08 13:09:42 +0600

    work section related update

--- app.js
+++ app.js
@@ -3,12 +3,18 @@ const navPos = navBar.getBoundingClientRect("nav-bar").top;
 
 let navbarLinks = document.querySelectorAll("nav ul li a");
 
+const htmlSec = document.querySelector(".html");
+
+const progressBars = document.querySelectorAll(".progress-bar");
+
+const display = document.querySelectorAll(".display");
+
 window.onload = () => {
   main();
 };
 
 function main() {
-  window.addEventListener("scroll", (event) => {
+  window.addEventListener("scroll", () => {
     let scrollPos = window.scrollY;
     if (scrollPos > navPos) {
       navBar.classList.add("bg-dark");
@@ -27,5 +33,62 @@ function main() {
         link.classList.remove("active");
       }
     });
+
+    const sectionPos = htmlSec.getBoundingClientRect().top;
+
+    const screenPos = window.innerHeight / 2;
+
+    if (sectionPos < screenPos) {
+      showProgress();
+    } else {
+      hideProgress();
+    }
   });
+
+  window.addEventListener("scroll", () => {});
+}
+
+let interval = 500;
+
+function showProgress() {
+  let startValue = 0;
+  let value;
+  let duration = Math.floor(interval / value);
+  progressBars.forEach((progressBar) => {
+    value = progressBar.ariaValueNow;
+    progressBar.style.width = `${value}%`;
+    progressBar.style.opacity = "1";
+  });
+
+  let counter = setInterval(function () {
+    startValue += 1;
+    display.forEach((d) => {
+      d.textContent = `${startValue}%`;
+    });
+
+    if (startValue == value) {
+      clearInterval(counter);
+    }
+  }, duration);
+}
+
+function hideProgress() {
+  let startValue = 0;
+  let value;
+  let duration = Math.floor(interval / value);
+  progressBars.forEach((progressBar) => {
+    value = progressBar.ariaValueNow;
+    progressBar.style.width = `0%`;
+    progressBar.style.opacity = "0";
+  });
+
+  let counter = setInterval(function () {
+    value -= 1;
+    display.forEach((d) => {
+      d.textContent = `${value}%`;
+    });
+    if (startValue == value) {
+      clearInterval(counter);
+    }
+  }, duration);
 }
