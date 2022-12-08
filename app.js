5210d634611d62b9a6a573f0378d49bb4fc34602
Naim Ahmad <naimahmad201103@gmail.com>
2022-12-09 05:59:42 +0600

    progress animation perfect

--- app.js
+++ app.js
@@ -1,94 +1,85 @@
+// Dom references
+
 const navBar = document.getElementById("nav-bar");
 const navPos = navBar.getBoundingClientRect("nav-bar").top;
 
 let navbarLinks = document.querySelectorAll("nav ul li a");
 
-const htmlSec = document.querySelector(".html");
-
-const progressBars = document.querySelectorAll(".progress-bar");
-
 const display = document.querySelectorAll(".display");
+
+const progressBar = document.querySelectorAll(".progress-bar");
+
+window.onscroll = () => {
+  progressBar.forEach((prog) => {
+    let scrollPos = window.scrollY;
+    // console.log(prog.offsetTop);
+  });
+};
+
+console.log(progressBar);
+
+// windows onload handler
 
 window.onload = () => {
   main();
 };
 
+// main function
+
 function main() {
+  navBarActive();
+  progressAnimation();
+
   window.addEventListener("scroll", () => {
-    let scrollPos = window.scrollY;
-    if (scrollPos > navPos) {
-      navBar.classList.add("bg-dark");
-    } else {
-      navBar.classList.remove("bg-dark");
-    }
-
-    navbarLinks.forEach((link) => {
-      let section = document.querySelector(link.hash);
-      if (
-        scrollPos + 150 > section.offsetTop &&
-        scrollPos + 150 < section.offsetTop + section.offsetHeight
-      ) {
-        link.classList.add("active");
-      } else {
-        link.classList.remove("active");
-      }
-    });
-
-    const sectionPos = htmlSec.getBoundingClientRect().top;
-
-    const screenPos = window.innerHeight / 2;
-
-    if (sectionPos < screenPos) {
-      showProgress();
-    } else {
-      hideProgress();
-    }
+    navBarActive();
+    progressAnimation();
   });
-
-  window.addEventListener("scroll", () => {});
 }
 
-let interval = 500;
+function progressAnimation() {
+  let scrollPos = window.scrollY;
 
-function showProgress() {
-  let startValue = 0;
-  let value;
-  let duration = Math.floor(interval / value);
-  progressBars.forEach((progressBar) => {
-    value = progressBar.ariaValueNow;
-    progressBar.style.width = `${value}%`;
-    progressBar.style.opacity = "1";
-  });
-
-  let counter = setInterval(function () {
-    startValue += 1;
-    display.forEach((d) => {
-      d.textContent = `${startValue}%`;
-    });
-
-    if (startValue == value) {
-      clearInterval(counter);
+  progressBar.forEach((prog) => {
+    if (scrollPos + 650 > prog.offsetTop) {
+      showProgress(prog);
+    } else {
+      hideProgress(prog);
     }
-  }, duration);
+  });
 }
 
-function hideProgress() {
-  let startValue = 0;
-  let value;
-  let duration = Math.floor(interval / value);
-  progressBars.forEach((progressBar) => {
-    value = progressBar.ariaValueNow;
-    progressBar.style.width = `0%`;
-    progressBar.style.opacity = "0";
-  });
+// Utility Function
 
-  let counter = setInterval(function () {
-    value -= 1;
-    display.forEach((d) => {
-      d.textContent = `${value}%`;
-    });
-    if (startValue == value) {
-      clearInterval(counter);
+function showProgress(prog) {
+  const value = prog.ariaValueNow;
+  prog.style.width = `${value}%`;
+  prog.style.opacity = "1";
+}
+
+function hideProgress(pro) {
+  pro.style.width = `0%`;
+  pro.style.opacity = "0";
+}
+
+function navBarActive() {
+  let scrollPos = window.scrollY;
+
+  if (scrollPos > navPos) {
+    navBar.classList.add("bg-dark");
+  } else {
+    navBar.classList.remove("bg-dark");
+  }
+
+  navbarLinks.forEach((link) => {
+    let section = document.querySelector(link.hash);
+
+    if (
+      scrollPos + 300 > section.offsetTop &&
+      scrollPos + 300 < section.offsetTop + section.offsetHeight
+    ) {
+      link.classList.add("active");
+    } else {
+      link.classList.remove("active");
     }
-  }, duration);
+  });
 }
